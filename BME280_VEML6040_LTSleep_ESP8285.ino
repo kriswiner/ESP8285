3a48df3bcfd9160d9d7dc08e58ad827ba82ad120
Kris Winer <kriswiner@users.noreply.github.com>
2018-02-20 19:01:47 -0800

    Update BME280_VEML6040_LTSleep_ESP8285.ino

--- BME280_VEML6040_LTSleep_ESP8285.ino
+++ BME280_VEML6040_LTSleep_ESP8285.ino
@@ -49,9 +49,9 @@ void      I2Cscan();
 void      writeByte(uint8_t address, uint8_t subAddress, uint8_t data);
 uint8_t   readByte(uint8_t address, uint8_t subAddress);
 void      readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * dest);
-uint32_t  readBME280Temperature();
-uint32_t  readBME280Pressure();
-uint16_t  readBME280Humidity();
+int32_t  readBME280Temperature();
+int32_t  readBME280Pressure();
+int16_t  readBME280Humidity();
 void      BME280Init();
 uint32_t  BME280_compensate_P(int32_t adc_P);
 int32_t   BME280_compensate_T(int32_t adc_T);
@@ -173,8 +173,8 @@ float redLight, greenLight, blueLight, ambientLight;
 
 float Temperature, Pressure, Humidity; // stores BME280 pressures sensor pressure and temperature
 float VBAT;  // battery voltage from ESP8285 ADC read
-uint32_t rawPress, rawTemp;   // pressure and temperature raw count output for BME280
-uint16_t rawHumidity;  // variables to hold raw BME280 humidity value
+int32_t rawPress, rawTemp;   // pressure and temperature raw count output for BME280
+int16_t rawHumidity;  // variables to hold raw BME280 humidity value
 
 // BME280 compensation parameters
 uint8_t  dig_H1, dig_H3, dig_H6;
@@ -448,25 +448,25 @@ void initWifi() {
   }
   }
     
-uint32_t readBME280Temperature()
+int32_t readBME280Temperature()
 {
   uint8_t rawData[3];  // 20-bit pressure register data stored here
   readBytes(BME280_ADDRESS, BME280_TEMP_MSB, 3, &rawData[0]);  
-  return (uint32_t) (((uint32_t) rawData[0] << 16 | (uint32_t) rawData[1] << 8 | rawData[2]) >> 4);
+  return (int32_t) (((int32_t) rawData[0] << 24 | (int32_t) rawData[1] << 16 | (int32_t) rawData[2] << 8) >> 12);
 }
 
-uint32_t readBME280Pressure()
+int32_t readBME280Pressure()
 {
   uint8_t rawData[3];  // 20-bit pressure register data stored here
   readBytes(BME280_ADDRESS, BME280_PRESS_MSB, 3, &rawData[0]);  
-  return (uint32_t) (((uint32_t) rawData[0] << 16 | (uint32_t) rawData[1] << 8 | rawData[2]) >> 4);
+  return (int32_t) (((int32_t) rawData[0] << 24 | (int32_t) rawData[1] << 16 | (int32_t) rawData[2] << 8) >> 12);
 }
 
-uint16_t readBME280Humidity()
+int16_t readBME280Humidity()
 {
   uint8_t rawData[3];  // 20-bit pressure register data stored here
   readBytes(BME280_ADDRESS, BME280_HUM_MSB, 2, &rawData[0]);  
-  return (uint16_t) (((uint16_t) rawData[0] << 8 | rawData[1]) );
+  return (int16_t) (((int16_t) rawData[0] << 8 | rawData[1]) );
 }
 
 
